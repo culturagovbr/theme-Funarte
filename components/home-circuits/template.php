8d8323ddf914e9f18060106a9da239c48c09cb0b
Laura Petrola <laupetrola@gmail.com>
2025-06-14 13:16:27 -0300

    versao final

--- components/home-circuits/template.php
+++ components/home-circuits/template.php
@@ -10,7 +10,7 @@ use MapasCulturais\i;
 ?>
 
 <?php
-$circuits_url = $app->view->asset('img/home/home-circuits/circuits.jpg', false);
+$circuits_url = $app->view->asset('img/home/home-circuits/circuits.gif', false);
 ?>
 
 <div class="custom-background">
