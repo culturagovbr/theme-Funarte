0eb890cc9ec9b49b0a7587405be542e3f3d6c9a7
Davi Pontes <davi.luciano1@gmail.com>
2025-07-22 16:34:51 +0000

    Implementacao da home de acordo com o novo design, incluindo responsividade, faltando a implementacao da fonte nova

--- components/home-register/template.php
+++ components/home-register/template.php
@@ -6,17 +6,28 @@
 
 use MapasCulturais\i;
 ?>
+
+<?php
+$banner_left_url = $app->view->asset('img/home/home-register/banner-left.png', false);
+?>
+
+<?php
+$banner_right_url = $app->view->asset('img/home/home-register/banner-right.png', false);
+?>
+
 <div class="home-register">
+   
     <div class="home-register__background">
         <div class="home-register__background--mask"></div>
     </div>
+    <img src="<?= $banner_left_url ?>" alt="Adorno esquerdo" class="decoration banner-left" />
     <div class="home-register__content">
-        <label class="home-register__content--title"><?= $this->text('title', i::__('Conecte-se à Rede das Artes! ')) ?></label>
-        <p class="home-register__content--description"><?= $this->text('description', i::__('Utilize as ferramentas desta plataforma. Divulgue suas atividades, acompanhe ações e fortaleça a rede artística no Brasil.')); ?>
+        <label class="home-register__content--title"><?= $this->text('title', i::__('Faça parte da Rede das Artes! ')) ?></label>
+        <p class="home-register__content--description"><?= $this->text('description', i::__('Cadastre seu portfólio, divulgue suas ações, acompanhe a agenda do seu território, conecte-se com outros agentes e fortaleça a Rede das Artes do Brasil.')); ?>
         </p>
         <a href="<?= $app->createUrl('autenticacao', 'register') ?>" class="button button--primary button--large button--icon">
             <?= i::__('Fazer Cadastro')?>
-            
         </a>
     </div>
+    <img src="<?= $banner_right_url ?>" alt="Adorno direito" class="decoration banner-right" />
 </div>
