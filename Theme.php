3775cf597993e22a46227c8ba850a714d8875f7c
Gabriel Urano <gabrielurano30@gmail.com>
2025-07-24 15:33:28 -0300

    feat: adiciona o clarity

--- Theme.php
+++ Theme.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Funarte;
+namespace MapasCulturais\Themes\Funarte;
 
 use MapasCulturais\App;
 use MapasCulturais\API;
@@ -20,6 +20,7 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme
 
         $app->hook('template(<<*>>.head):end', function () {
             $this->part('google-analytics--script');
+            $this->part('clarity--script');
         });
         $app->hook("ApiQuery(<<project|opportunity|event|space>>).params", function(&$api_params) use($app) {
             if($subsite = $app->subsite){
