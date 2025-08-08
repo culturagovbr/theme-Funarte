4c8a360cd37bac0297952f0ad4513146d580f165
Edson Coelho <edcoelho.dev@gmail.com>
2025-08-08 01:23:35 -0300

    Atualiza os textos da home. (#60)

--- components/home-entities/template.php
+++ components/home-entities/template.php
@@ -13,32 +13,33 @@ $entities = [
         'image' => 'img/cards/agenda_bg.png',
         'title' => 'Agenda',
         'route' => 'search/events',
-        'description' => 'Aqui você pode acompanhar as ações da Rede das Artes: apresentações artísticas, ações de pesquisa, reflexão, intercâmbios, criação, oficinas, cursos, entre outras. Uma agenda cultural que integra iniciativas artísticas de todo o Brasil.',
+        'description' => 'Aqui é espaço de agenda! Você pode acompanhar as ações incluídas na Rede das Artes: apresentações artísticas, ações de pesquisa, reflexão, intercâmbios, criação, oficinas, cursos, entre outras. Inclua e divulgue sua atividade nesta agenda cultural que integra iniciativas artísticas de todo o Brasil.',
     ],
     'opportunities' => [
         'image' => 'img/cards/oportunidades_bg.png',
         'title' => 'Oportunidades',
         'route' => 'search/opportunities',
-        'description' => 'Acesso a inúmeras possibilidades de conexão com a Rede das Artes. Aqui você encontra atividades com inscrições abertas, como convocatórias, oficinas, intercâmbios e residências artísticas, editais, eventos e demais oportunidades acessíveis. Você também pode criar e ofertar oportunidades para outros agentes da Rede.',
+        'description' => 'Aqui você encontra atividades e chamadas com inscrições abertas, como editais, intercâmbios, residências artísticas e outras oportunidades disponíveis. Você também pode criar e divulgar novas oportunidades para outros agentes da Rede.',
     ],
     'agents' => [
         'image' => 'img/cards/agentes_bg.png',
         'title' => 'Agentes',
         'route' => 'search/agents',
-        'description' => 'Conheça artistas, grupos, coletivos e outros agentes artísticos cadastrados na Plataforma Rede das Artes. Aqui você conhece os participantes da Rede das Artes e pode se inscrever para fazer parte. São agentes das artes, realizadores de festivais, grupos e coletivos de artes visuais, circo, dança, música, teatro e artes integradas.',
+        'description' => 'Aqui você conhece participantes da Rede das Artes e pode se inscrever para fazer parte. São agentes das artes, realizadores de festivais, grupos e coletivos de artes visuais, circo, dança, música, teatro e artes integradas. Conheça e se apresente!',
     ],
     'spaces' => [
         'image' => 'img/cards/espacos_bg.png',
         'title' => 'Espaços',
         'route' => 'search/spaces',
-        'description' => 'Aqui você encontra espaços culturais e artísticos situados no território nacional que atuam de forma contínua e estruturada e conheça as ações ofertadas e possibilidades de parcerias. São casas de espetáculos e shows, galpões, centros coreográficos, salas de concertos, centros de referência, teatros, lonas circenses e centros culturais. Cadastre também os espaços onde desenvolve suas atividades.',
+        'description' => 'Aqui você pode inscrever o seu espaço artístico-cultural e divulgar suas ações para o público e possíveis parceiros. Também é possível utilizar o mapa e conhecer uma rede de espaços que atuam de forma contínua e estruturante em todo o país — como casas de espetáculos, galpões, centros coreográficos, salas de concerto, centros de referência, teatros, galerias, lonas circenses e centros culturais.',
     ]
 ];
 ?>
 
 <div class="home-entities">
   <div class="home-entities__content">
-    <div class="home-entities__cards">
+    <label class="home-entities__content--title"><?= $this->text('title', i::__('ENCONTRE AS INFORMAÇÕES SOBRE A REDE DAS ARTES BRASILEIRAS')) ?></label>
+    <div class="home-entities__content--cards">
       <?php foreach ($entities as $key => $entity): ?>
         <?php $imageUrl = $app->view->asset($entity['image'], false); ?>
         <div v-if="global.enabledEntities.<?= $key ?>" class="card">
--- components/home-prosas-notices/template.php
+++ components/home-prosas-notices/template.php
@@ -17,21 +17,21 @@ $this->import('
 <div class="home-prosas-notices">
     <div class="home-prosas-notices__header">
         <div class="home-prosas-notices__header--title">
-            <label><?= $this->text('title', i::__('FOMENTO ÀS ARTES')) ?></label>
+            <label><?= $this->text('title', i::__('CONFIRA EDITAIS E CONVOCATÓRIAS DA FUNARTE')) ?></label>
         </div>
         <div class="home-prosas-notices__header--icon">
             <img src="<? echo $app->view->asset('img/home/home-prosas-notices/icon-bandeira.png', false); ?>" alt="">
         </div>
         <div class="home-prosas-notices__header--description">
-            <label><?= $this->text('description', i::__('Acompanhe as oportunidades artísticas disponíveis no momento. Aqui você encontra editais com inscrições abertas para projetos, ações artísticas e iniciativas de internacionalização.')) ?></label>
+            <label><?= $this->text('description', i::__('Acesse os editais e convocatórias lançados pela Funarte com inscrições abertas e visualize os chamamentos já finalizados.')) ?></label>
         </div>
     </div>
     <div class="home-prosas-notices__content">
         <mc-tabs>
-            <mc-tab label="<?= $this->text('title', i::__('Editais com inscrições abertas')) ?>" slug="openNotices">
+            <mc-tab label="<?= $this->text('title', i::__('Inscrições abertas')) ?>" slug="openNotices">
                 <prosas-listagem-editais per-page=5 not-show="pesquisa, areas_interesse" :edital-ids="idList" :client-id="clientId"></prosas-listagem-editais>
             </mc-tab>
-            <mc-tab label="<?= $this->text('title', i::__('Editais com inscrições encerradas')) ?>" slug="closedNotices">
+            <mc-tab label="<?= $this->text('title', i::__('Inscrições encerradas')) ?>" slug="closedNotices">
                 <prosas-listagem-editais encerrados="required" per-page=5 not-show="pesquisa, areas_interesse" :edital-ids="idList" :client-id="clientId"></prosas-listagem-editais>
             </mc-tab>
         </mc-tabs>
