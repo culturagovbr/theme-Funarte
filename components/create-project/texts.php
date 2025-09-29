9ef132abe9beb6e4ae9e7df89e3ec0916258426f
Gabriel Urano <gabrielurano30@alu.ufc.br>
2025-09-29 12:07:08 -0300

    feat: improvements for iniciativa create/edit modal (#77)

---
+++ components/create-project/texts.php
@@ -0,0 +1,8 @@
+<?php
+use MapasCulturais\i;
+
+return [
+    'projetoCriado' => i::__('Sua iniciativa foi publicada!'),
+    'criarProjeto' => i::__('Criar Iniciativa'),
+    'criarRascunho' => i::__('Sua iniciativa foi salva como rascunho! Você pode acessá-la em Minhas iniciativas no painel de controle.'),
+];
